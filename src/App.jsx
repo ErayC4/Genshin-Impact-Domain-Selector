8fb489d1ec47c8deea5357c49d53ff95a115a492
ErayC4 <eray04.coskun@gmail.com>
2024-02-19 19:54:44 +0100

    top bar complete

--- src/App.jsx
+++ src/App.jsx
@@ -6,37 +6,40 @@ function App() {
     <div className="relative h-screen w-screen">
       <div
         className="absolute inset-0 bg-cover bg-center"
-        style={{ backgroundImage: "url(/images/bgImage.jpg)" }}
+        style={{ backgroundImage: "url(/images/background.png)" }}
       ></div>
       <div className="absolute inset-0 backdrop-blur-lg">
-        <div className="w-full bg-black bg-opacity-50 h-20">
-          <div className="mx-8">
-            <img className="w-12 h-12" src="/svg/genshin2.svg" alt="" />
-            <p className="text-xl text-[#d2bd8e]">Domain Entrance</p>
-          </div>
-
-          <div className="flex">
-            <div className="flex items-center bg-gray-800 rounded-full">
-              <img
-                className="w-10 h-10"
-                src="/images/Item_Fragile_Resin.webp"
-                alt=""
-              />
-              <p className="text-white text-xl">134/160</p>
-              <img
-                className="w-8 h-8 bg-white p-2 rounded-full"
-                src="/svg/genshinplus.svg"
-                alt=""
-              />
+        <div className="flex bg-black bg-opacity-55 h-20">
+          <div className="w-full flex items-center justify-between mx-12">
+            {/*Domain rechts container*/}
+            <div className="flex items-center gap-8">
+              <img className="w-12 h-12" src="/svg/genshin2.svg" alt="" />
+              <p className="text-xl text-[#d2bd8e]">Domain Entrance</p>
             </div>
 
-            <button>
-              <img
-                className="bg-white w-12 h-12 rounded-full border-4 border-gray-500"
-                src="/svg/genshinx.svg"
-                alt=""
-              />
-            </button>
+            {/*links container mit resin und x button */}
+            <div className="flex gap-8 h-8 items-center">
+              {/*resin refill*/}
+              <div className="flex items-center bg-gray-800 border-2 gap-2 border-gray-700 rounded-full">
+                <img
+                  className="w-10 h-10"
+                  src="/images/Item_Fragile_Resin.webp"
+                  alt=""
+                />
+                <p className="text-white text-xl">9/160</p>
+                <button className="w-6 h-6 bg-white p-1.5 rounded-full mr-2">
+                  <img src="/svg/genshinplus.svg" alt="" />
+                </button>
+              </div>
+
+              <button>
+                <img
+                  className="bg-white w-12 h-12 rounded-full border-4 border-gray-500"
+                  src="/svg/genshinx.svg"
+                  alt=""
+                />
+              </button>
+            </div>
           </div>
         </div>
       </div>
