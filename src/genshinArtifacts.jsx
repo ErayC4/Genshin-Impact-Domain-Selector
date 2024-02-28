4b4ecf3f6f60db023995c0b425c6fb67f2ec2449
ErayC4 <eray04.coskun@gmail.com>
2024-02-28 23:30:07 +0100

    Made small adjustments to the horizontal scrollbar, now it does move the correct div

--- src/genshinArtifacts.jsx
+++ src/genshinArtifacts.jsx
@@ -1,7 +1,11 @@
 import React, { useRef, useState } from "react";
 import Artifacts from "./artifacts";
 
-function GenshinImpactArtifact({ clickedIndex, domainRewardInformation, domainMaxLvL }) {
+function GenshinImpactArtifact({
+  clickedIndex,
+  domainRewardInformation,
+  domainMaxLvL,
+}) {
   const containerRef = useRef(null);
   const [isDragging, setIsDragging] = useState(false);
   const [startX, setStartX] = useState(0);
@@ -34,30 +38,46 @@ function GenshinImpactArtifact({ clickedIndex, domainRewardInformation, domainMa
   return (
     <div>
       <p className="text-white text-xl py-2">Possible Rewards: </p>
-      {domainRewardInformation && Array.isArray(domainRewardInformation.rewards) &&
+      {domainRewardInformation &&
+        Array.isArray(domainRewardInformation.rewards) &&
         domainRewardInformation.rewards.map((reward, index) => (
           <div key={index}>
             <div>
               {reward.details.map((detail, detailIndex) => (
-                <div key={detailIndex}>
+                <div key={detailIndex} className="overflow-hidden gap-4" ref={containerRef}
+                onMouseDown={handleMouseDown}
+                onMouseMove={handleMouseMove}
+                onMouseUp={handleMouseUp}
+                onMouseLeave={handleMouseUp}>
                   {clickedIndex === detail.level - 1 && (
-                    <div style={styles} className="flex gap-4 overflow-hidden" ref={containerRef}
-                    onMouseDown={handleMouseDown}
-                    onMouseMove={handleMouseMove}
-                    onMouseUp={handleMouseUp}
-                    onMouseLeave={handleMouseUp}>
+                    <div
+                      style={styles}
+                      className="flex gap-4"
                       
+                    >
                       <div>
-                        <img className="w-24 rounded-lg absolute" src="/images/domainRewards/mora.jpg" alt="" />
-                        <p className="relative text-white text-center w-24 mt-16 rounded-b-lg bg-gray-700 bg-opacity-50">{detail.mora}</p>
+                        <img
+                          className="w-24 rounded-lg absolute"
+                          src="/images/domainRewards/mora.jpg"
+                          alt=""
+                        />
+                        <p className="relative text-white text-center w-24 mt-16 rounded-b-lg bg-gray-700 bg-opacity-50">
+                          {detail.mora}
+                        </p>
                       </div>
 
                       <div>
-                        <img className="w-24 h-24 rounded-lg absolute" src="/images/domainRewards/companionShipXP.jpg" alt="" />
-                        <p className="relative text-white text-center w-24 mt-[4.25rem] rounded-b-lg bg-gray-700 bg-opacity-50">{detail.companionshipExperience}</p>
+                        <img
+                          className="w-24 h-24 rounded-lg absolute"
+                          src="/images/domainRewards/companionShipXP.jpg"
+                          alt=""
+                        />
+                        <p className="relative text-white text-center w-24 mt-[4.25rem] rounded-b-lg bg-gray-700 bg-opacity-50">
+                          {detail.companionshipExperience}
+                        </p>
                       </div>
-                      <div className="flex gap-4">
-                        {detail.drops && Array.isArray(detail.drops) &&
+                      {detail.drops &&
+                          Array.isArray(detail.drops) &&
                           detail.drops.map((drop, dropIndex) => (
                             <div key={dropIndex}>
                               <Artifacts
@@ -68,7 +88,7 @@ function GenshinImpactArtifact({ clickedIndex, domainRewardInformation, domainMa
                               />
                             </div>
                           ))}
-                      </div>
+                        
                     </div>
                   )}
                 </div>
